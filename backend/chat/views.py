ecd5b3432bd559dd2c5e1b06437a66aeb04a0c72
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2024-01-22 19:17:25 +0300

    prompt engineering iteration 1

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -11,12 +11,14 @@ def chat_with_openai(request):
     if request.method == 'POST':
         user_input = request.POST.get('message', '')
 
-        openai.api_key = "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"
+        openai.api_key = "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"
 
         try:
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": user_input}]
+                messages=[
+                        {"role": "system", "content": "Convert the user message into a book description format to enhance the accuracy of vector search on our book database. The goal is to provide users with books that match their preferences more precisely. Please ensure that the book description format captures the essence of the user message, allowing for a more accurate search. The format should effectively convey the key elements of the user message in a way that enables the database to match users with books that align with their preferences."},
+                        {"role": "user", "content": user_input}]
             )
             ai_response = response.choices[0].message['content']
             return JsonResponse({'response': ai_response})
