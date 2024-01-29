6db4127e08aa099cbccc7e353ec69c22f4b686c9
Burak Mat <burakk.mat@gmail.com>
2024-01-29 23:43:18 +0300

    change pdf_reader location

--- backend/pdf_reader.py
+++ backend/pdf_reader.py
@@ -1,9 +1,9 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-book_path = "../../sample_book.pdf"
+book_path = "../sample_book.pdf"
 
 loader = PyPDFLoader(book_path)
 
 pages = loader.load_and_split()
 
-print(pages[2])
+print(type(pages[2]))
